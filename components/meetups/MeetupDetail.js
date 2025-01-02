b4571fccf79c371fd1e96f1f9ef4f04cde45f600
AmanKalabay <149198402+Amansdfg@users.noreply.github.com>
2025-01-02 13:44:30 +0500

    [XCLOUD-4240] add first commit

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,6 +1,7 @@
+import classes from "./MeetupDetail.module.css"
 function MeetupDetail(props){
     return(
-        <>
+        <section className={classes.detail}>
             <img
             src={props.image}
             alt={props.title}
@@ -9,7 +10,7 @@ function MeetupDetail(props){
             <h1>{props.title}</h1>
             <address>{props.address}</address>
             <p>{props.description}</p>
-        </>
+        </section>
     )
 }
 export default MeetupDetail
